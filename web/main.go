a309b14bcb7659cba928e3f2000bc5a524975454
Ljolan <2018214546@qq.com>
2021-01-17 10:45:54 +0800

    feat:QAca-Ga

--- web/main.go
+++ web/main.go
@@ -1,11 +1,15 @@
 package main
 
-import "github.com/kataras/iris/v12"
+import (
+	"fmt"
+	"github.com/kataras/iris/v12"
+)
 
-func main()  {
+func main() {
 	app := iris.New()
 
 	app.Get("/", func(ctx iris.Context) {
+		fmt.Println(ctx.Request().Header.Get("Accept"))
 		ctx.WriteString("你好")
 	})
 	app.Run(iris.Addr(":80"))
